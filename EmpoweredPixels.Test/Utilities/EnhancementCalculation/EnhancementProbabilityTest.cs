a869975369a2cc953fbd74519c81a8d6adbc0dc5
Alex <alexheroldn81@gmail.com>
2019-10-31 11:59:39 +0000

    Version 0.14.15

---
+++ EmpoweredPixels.Test/Utilities/EnhancementCalculation/EnhancementProbabilityTest.cs
@@ -0,0 +1,43 @@
+﻿using EmpoweredPixels.Interfaces.Equipment;
+using EmpoweredPixels.Utilities.EnhancementCalculation;
+using Moq;
+using Xunit;
+
+namespace EmpoweredPixels.Test.Utilities.EnhancementCalculation
+{
+  public class EnhancementProbabilityTest
+  {
+    private readonly IEnhancementProbability probability = new EnhancementProbability();
+
+    [Theory]
+    [InlineData(1, 0.9)]
+    [InlineData(2, 0.8)]
+    [InlineData(3, 0.7)]
+    [InlineData(4, 0.6)]
+    [InlineData(5, 0.5)]
+    [InlineData(6, 0.5)]
+    [InlineData(7, 0.5)]
+    [InlineData(8, 0.5)]
+    [InlineData(9, 0.5)]
+    [InlineData(10, 0.5)]
+    [InlineData(20, 0.5)]
+    [InlineData(100, 0.5)]
+    public void ShouldEnhance(int enhancement, float expectedProbability)
+    {
+      var enhancable = Mock.Of<IEnhancable>();
+
+      enhancable.Enhancement = enhancement;
+
+      const int tries = 10000;
+      float successCount = 0;
+      for (int i = 0; i < tries; i++)
+      {
+        successCount += probability.IsSuccess(enhancable) ? 1 : 0;
+      }
+
+      var actual = successCount / tries;
+
+      Assert.Equal(expectedProbability, actual, 1);
+    }
+  }
+}
